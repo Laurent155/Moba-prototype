30cf661bc1b23c48fbc42da2907f557e12008bf1
Laurent155 <laurentwu155@hotmail.fr>
2021-05-02 03:27:31 +0100

    added player controller, camera controller, with player walking animation.
    navigation system using NavMesh, NavMeshAgent. Camera following player with a fixed offset smoothly.  added walking - running animation.

---
+++ Moba challenge/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public Vector3 camOffset = new Vector3(0f, 12f, -10f);
+    private Transform target;
+    private float camFOV;
+    public float zoomSpeed;
+    private Camera cam;
+    private float mouseScroll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        camFOV = cam.fieldOfView;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // this moves the camera smoothly when the player moves, with an offset
+        Vector3 pos = target.position + camOffset;
+        transform.position = Vector3.Slerp(transform.position, pos, 0.5f);
+
+        // this controls the zooming of the camera with mouse scroll
+        // using GetAxis to get 1 or -1 (direction), and scale it with zoom speed
+        mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+        camFOV -= mouseScroll * zoomSpeed;
+        camFOV = Mathf.Clamp(camFOV, 30, 60);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, camFOV, zoomSpeed);
+    }
+}
---
+++ Moba challenge/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            // {
+            //     Debug.Log("The ray hit at: " + hit1.point);
+            // }
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        float speed = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("Speed", speed, 0.1f, Time.deltaTime);
+
+    }
+
+}
